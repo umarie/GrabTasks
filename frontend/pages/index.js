0366895435127dfdc46bacd2bdb4ff4dfa491a5c
harris <harryaamir073@gmail.com>
2023-04-04 21:36:03 +0500

    Orders_extended-Harris

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Orders_extended from './freelancer/Orders_extended'
 
 const inter = Inter({ subsets: ['latin'] })
 
