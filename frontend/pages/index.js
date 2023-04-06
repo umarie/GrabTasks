78b944ed8702260c941fe265a74a1ce2e5355a39
MFurqanCourse <i200413@nu.edu.pk>
2023-04-06 22:00:08 +0500

    Revert "Merge branch 'main' of https://github.com/umarie/GrabTasks"
    This reverts commit fda52b24581b36c2b117a9c62039add617674e30.

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,14 +2,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Orders_extended from './freelancer/Orders_extended'
+import Header from '@/Components/layout/header'
+import LandingComp from '@/Components/layout/landingpgcomp'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      <Orders_extended/>
-    </>
+
+  <>
+    <h1>Hello</h1>
+      {/* <Header/>
+      <LandingComp/> */}
+    </> 
   )
 }
