fda52b24581b36c2b117a9c62039add617674e30
MFurqanCourse <i200413@nu.edu.pk>
2023-04-06 21:52:29 +0500

    Merge branch 'main' of https://github.com/umarie/GrabTasks

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,18 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Header from '@/Components/layout/header'
-import LandingComp from '@/Components/layout/landingpgcomp'
+import Orders_extended from './freelancer/Orders_extended'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-
-  <>
-    <h1>Hello</h1>
-      {/* <Header/>
-      <LandingComp/> */}
-    </> 
+    <>
+      <Orders_extended/>
+    </>
   )
 }
