1b29a9600d07f405ac2e0540ac16223ae02cc671
harris <harryaamir073@gmail.com>
2023-04-28 17:42:06 +0500

    Freelancer-Order

--- frontend/Components/utilities/Orders.js
+++ frontend/Components/utilities/Orders.js
@@ -1,4 +1,4 @@
-import react from "react";
+import react, {useState, useEffect} from "react";
 import Image from "next/image";
 import img1 from "/public/Images/Orders_Page/orders1.png";
 import avatar from "/public/Images/Smavatar.png";
@@ -6,6 +6,13 @@ import clock from "/public/Images/Orders_Page/clock.png";
 import dollar from "/public/Images/Orders_Page/dollar.png";
 import submit from "/public/Images/Orders_Page/submit.png";
 import cancel from "/public/Images/Orders_Page/cancel.png";
+import outsource from "/public/Images/Orders_Page/outsource.png";
+import flag from "/public/Images/Orders_Page/milestoneflag.png";
+import milestone from "/public/Images/Orders_Page/milestone.png";
+import milestone2 from "/public/Images/Orders_Page/milestone2.png";
+import milestone3 from "/public/Images/Orders_Page/milestone3.png";
+import bar from "/public/Images/Orders_Page/bar.png";
+
 // const order={
 //     title:"Design a custom built Website for you.",
 //     description:"Design an Ecommerce store with built-in API’s etc..",
@@ -15,79 +22,242 @@ import cancel from "/public/Images/Orders_Page/cancel.png";
 //     onSumbit:()=>{console.log("Hello1")},
 //     onCancel:()=>{console.log("hello2")}
 // }
+let pcClassname="flex flex-row mx-10 gap-5 pt-8 border-t-[0.5px] max-h-[400px] border-black mt-4 mb-4"
+let phoneClassname="flex flex-col mx-4 gap-5 pt-8 border-t-[0.5px] border-black mt-4 mb-4 "
 const Orders = (props) => {
   let order = props.order;
+  const [phone, setPhone]=useState()
+
+    useEffect(() => {
+      chekPhone()
+    }, [])
+    
+
+    const chekPhone=()=>{
+        if(window.innerWidth >=1024)
+        {
+            setPhone(false);
+        }   
+        else setPhone(true)
+      }
+      if(typeof window==="object")
+        window.addEventListener("resize",chekPhone)
   return (
     <>
-      <div className="flex flex-row mx-10 gap-5 pt-8 border-t-[0.5px] border-black mt-4 mb-4">
-        <div className="basis-[12%]">
-          <Image
+    <div className={phone?phoneClassname:pcClassname}> 
+    <div className="flex flex-row gap-[5%] basis-[55%] max-w-[800px]">
+    <div className={(phone)?"min-w-[160px] max-h-[115px] ":"w-[50%] 2xl:max-h-[350px] 2xl:max-w-[483px] 2xl:ml-[8%]"}>
+    <Image
             src={order.img1}
             alt="Picture of the author"
-            width={"145px"}
-            height={"166px"}
+            width="100%" 
+            height="100%" 
+            layout="responsive" 
+            objectFit="contain"
           />
-        </div>
-        <div className="basis-[35%] ">
-          <p className="font-bold text-xl"> {order.title}</p>
-          <p className="mt-4 text-base mb-3">{order.description}</p>
-          <p className="text-base font-bold inline">From: {order.fromName} </p>
+    </div>
+    <div className="w-[80%]">
+    <p className="font-bold text-xl 2xl:text-5xl"> {order.title}</p>
+          <p className="mt-[2%] text-base 2xl:text-3xl">{order.description}</p>
+          <p className="text-base font-bold inline 2xl:text-3xl">From: {order.fromName} </p>
+
           <Image
             className="inline"
             src={avatar}
             alt="pic"
-            width={"25px"}
-            height={"25px"}
+            width={"25%"}
+            height={"25%"}
           />
+          <p className=" block text-base font-bold mt-[2%] 2xl:text-3xl">Due: {order.due} </p>
+    </div>
+    </div>
+    
+        {/* <div className={(phone)?"":"basis-[21%]"}>
+          <Image
+            src={order.img1}
+            alt="Picture of the author"
+            width="100%" 
+            height="100%" 
+            layout="responsive" 
+            objectFit="contain"
+            className={(phone)?"min-w-[150px] ":"2xl:max-h-[350px] 2xl:max-w-[483px] 2xl:ml-[8%]"}
+          />
+          {phone && (<div className="inline max-w-[800px] 2xl:ml-[8%]" >
+          <p className="font-bold 2xl:text-5xl"> {order.title}</p>
+          <p className="mt-4 text-base mb-3 2xl:text-3xl">{order.description}</p>
+          <p className="text-base font-bold inline 2xl:text-3xl">From: {order.fromName} </p>
+
+          <Image
+            className="inline"
+            src={avatar}
+            alt="pic"
+            width={"25%"}
+            height={"25%"}
+          />
+          <p className=" block text-base font-bold mt-2 2xl:text-3xl">Due: {order.due} </p>
+        </div>)}
         </div>
-        <div className="basis-[30%] flex flex-col gap-2 bg-[#122620] rounded-lg pt-5 ">
-          <div className="flex flex-center text-wrap">
-            <Image
+        
+        <div className="max-w-[800px] 2xl:ml-[8%]" >
+        {!phone && (<> <p className="font-bold 2xl:text-5xl"> {order.title}</p>
+          <p className="mt-4 text-base mb-3 2xl:text-3xl">{order.description}</p>
+          <p className="text-base font-bold inline 2xl:text-3xl">From: {order.fromName} </p>
+
+          <Image
+            className="inline"
+            src={avatar}
+            alt="pic"
+            width={"25%"}
+            height={"25%"}
+          />
+          <p className=" block text-base font-bold mt-2 2xl:text-3xl">Due: {order.due} </p></>)}
+         
+        </div> */}
+        <div className="basis-[35%] max-w-[600px] max-h-[350px] flex flex-col gap-5 bg-[#122620] rounded-lg pt-5 2xl:ml-[80px] sm:max-w-[550px]">
+          <div className="flex flex-center text-wrap align-middle">
+            <div  className="ml-[6%] mt-[2%] w-[8%] h-[8%] inline 2xl:w-[10%]" >
+             <Image
               src={clock}
-              width={"37px"}
-              height={"37px"}
+              width={"100%"}
+              height={"100%"}
               alt="clock"
-              className="ml-8 inline"
-            />
-            <p className="text-white inline font-bold text-2xl ml-4">
+             
+              layout="responsive" 
+              objectFit="contain"
+            /></div>
+      
+            <p className="text-white inline font-bold text-2xl ml-[5%] mt-[4%] 2xl:text-5xl">
               {order.remainingTime}
             </p>
-            <p className="text-white inline  text-base ml-2 mt-2 ">remaining</p>
+            <p className="text-white inline  text-base ml-[2%] mt-[4.5%] 2xl:text-3xl">remaining</p>
           </div>
-          <div className="flex flex-center">
+          <div className="flex flex-center align-middle">
+            <div className="ml-[8%] mt-[2%] w-[6%] h-[6%]">
             <Image
               src={dollar}
-              width={"37px"}
-              height={"37px"}
-              alt="clock"
-              className="ml-10"
+              width={"100%"}
+              height={"100%"}
+              alt="dollar"
+              layout="responsive" 
+              objectFit="contain"
             />
-            <p className="text-white inline font-bold text-2xl ml-4">
+            </div>
+            <p className="text-white inline font-bold text-2xl ml-[6%] mt-[3%] 2xl:text-5xl">
               {order.amount}
             </p>
           </div>
+          <div className="flex flex-center align-middle">
+            <div  className="ml-[8%] mt-[2%] w-[6%] h-[6%] ">
+            <Image
+              src={flag}
+              width={"100%"}
+              height={"100%"}
+              alt="falg"
+              layout="responsive" 
+              objectFit="contain"
+            />
+            </div>
+            {(order.milestone==="milestone") &&
+            <Image src={milestone} width={"50%"} height={"10%"} alt="falg"  className="mx-5 mb-4"/>}
+             {(order.milestone==="milestone2") &&
+            <Image src={milestone2} width={"50%"} height={"10%"} alt="falg"  className="mx-5 mb-4"/>}
+            {(order.milestone==="milestone3") &&
+            <Image src={milestone3} width={"50%"} height={"10%"} alt="falg"  className="mx-5 mb-4"/>}
+          
+          </div>
         </div>
-        <div className="basis-[15%]">
-          <div className="flex flex-col gap-4">
+        <div className="basis-[2%] min-w-[70px] max-w-[500px]">
+        {(phone)?(
+        <div className="flex flex-row gap-4 mx-auto" >
             <div
-              className="flex flex-row gap-3 border-[3px] border-black w-[100%] p-3 rounded-lg"
+              className="border-[3px] bg-[#122620] border-[#122620]  w-[100%] p-3 rounded-lg "
               role="button"
               onClick={order.onSumbit}
             >
-              <Image src={submit} width={"23px"} height={"23px"} alt="sumbit" />
-              <p>Submit Work</p>
+              <Image
+                src={submit}
+                width={"23px"}
+                height={"23px"}
+                alt="sumbit"
+                className="m-auto"
+              />
             </div>
             <div
-              className="flex flex-row gap-3 border-[3px] border-[#E21B1B] w-[100%] p-3 rounded-lg"
+              className="border-[3px]  bg-[#122620] border-[#122620] w-[100%]  p-3 rounded-lg "
               role="button"
               onClick={order.onCancel}
             >
-              <Image src={cancel} width={"23px"} height={"23px"} alt="sumbit" />
-              <p>Cancel Order</p>
+              <Image
+                src={outsource}
+                width={"23px"}
+                height={"23px"}
+                alt="outsource"
+                className="m-auto"
+              />
             </div>
-          </div>
+            <div
+              className="flex flex-row gap-3 border-[3px] border-[#E21B1B] w-[100%] p-4 rounded-lg"
+              role="button"
+              onClick={order.onCancel}
+            >
+              <Image
+                src={cancel}
+                width={"23px"}
+                height={"23px"}
+                alt="cancel"
+                className="m-auto"
+              />
+            </div>
+          </div>):(<div className="flex flex-col gap-4 " >
+            <div
+              className="border-[3px] bg-[#122620] border-[#122620]  w-[100%] p-[25%] rounded-lg 2xl:w-[100px]"
+              role="button"
+              onClick={order.onSumbit}
+            >
+              <Image
+                src={submit}
+                width="100%"
+                height="100%"
+                alt="sumbit"
+                className="m-auto"
+                layout="responsive" 
+                objectFit="contain"
+              />
+            </div>
+            <div
+              className="border-[3px]  bg-[#122620] border-[#122620] w-[100%]  p-[25%] rounded-lg 2xl:w-[100px]"
+              role="button"
+              onClick={order.onCancel}
+            >
+              <Image
+                src={outsource}
+                width={"100%"}
+                height={"100%"}
+                alt="outsource"
+                className="m-auto"
+                layout="responsive" 
+                objectFit="contain"
+              />
+            </div>
+            <div
+              className="flex flex-row gap-3 border-[3px] border-[#E21B1B] w-[100%] p-[25%] rounded-lg 2xl:w-[100px] "
+              role="button"
+              onClick={order.onCancel}
+            >
+              <Image
+                src={cancel}
+                width={"100%"}
+                height={"100%"}
+                alt="cancel"
+                className="m-auto"
+                layout="responsive" 
+                objectFit="contain"
+              />
+            </div>
+          </div>)}
         </div>
       </div>
+ 
     </>
   );
 };
--- frontend/pages/freelancer/Orders_Page.js
+++ frontend/pages/freelancer/Orders_Page.js
@@ -1,13 +1,17 @@
-let all = 17;
+let All = 17;
 let Cancelled = 1;
 let Completed = 12;
 let Active = 2;
-import { useState } from "react";
+let OutSourced=2;
+import { useState, useEffect } from "react";
 import Orders from "@/Components/utilities/Orders";
 import Image from "next/image";
 import arrow from "/public/Images/Orders_Page/arrow.png";
 import img1 from "/public/Images/Orders_Page/orders1.png";
 import img2 from "/public/Images/Orders_Page/img2.png";
+import milestone from "/public/Images/Orders_Page/milestone.png";
+import milestone2 from "/public/Images/Orders_Page/milestone2.png";
+import milestone3 from "/public/Images/Orders_Page/milestone3.png";
 const order = {
   title: "Design a custom built Website for you.",
   description: "Design an Ecommerce store with built-in API’s etc..",
@@ -15,6 +19,8 @@ const order = {
   remainingTime: "04 : 17 : 39",
   amount: "405$",
   img1: img1,
+  milestone:"milestone",
+  due:"12-2-2023",
   onSumbit: () => {
     console.log("Hello1");
   },
@@ -29,6 +35,8 @@ const order2 = {
   remainingTime: "04 : 17 : 39",
   amount: "405$",
   img1: img2,
+  milestone:"milestone2",
+  due:"18-2-2023",
   onSumbit: () => {
     console.log("Hello1");
   },
@@ -40,16 +48,41 @@ const Orders_Page = () => {
   const clickHandler = (event) => {
     console.log(event.target.id);
     setcategory(event.target.id);
+    setmenu()
   };
   const [category, setcategory] = useState("Active");
+  const [menuClass, setClass]=useState({open:"false",class:"hidden absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"})
+  const [phone, setPhone]=useState()
+
+  const setmenu=()=>{
+    if(menuClass.open=="true")
+    setClass({open:"false",class:"hidden absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"})
+    else setClass({open:"true",class:"absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"})
+  }
+  useEffect(() => {
+    chekPhone()
+  }, [])
+  
+
+  const chekPhone=()=>{
+      if(window.innerWidth >=1024)
+      {
+          setPhone(false);
+      }   
+      else setPhone(true)
+    }
+    if(typeof window==="object")
+      window.addEventListener("resize",chekPhone)
+
   return (
     <>
-      <button className=" flex flex-row gap-4 bg-black ml-12 mt-28 pt-3 pb-4 pr-4 pl-3 text-xl font-bold text-base rounded text-white">
-        <Image width={24} height={24} src={arrow} alt="arrow" />
+      {!phone?<div>
+        <button className=" flex flex-row gap-4 bg-black ml-12 mt-28 pt-3 pb-4 pr-4 pl-3 font-bold text-base rounded text-white">
+        <Image width={"24%"} height={"24%"} src={arrow} alt="arrow" />
         Dashboard
       </button>
       <h1 className="text-black mt-6 ml-12 font-bold text-3xl">Orders</h1>
-      <div className="flex flex-row gap-36 ml-12 mt-6 text-black font-bold text-xl">
+      <div className="flex flex-row gap-36 ml-12 mt-6 text-black font-bold text-xl ">
         <h2
           role="button"
           id="Active"
@@ -57,6 +90,14 @@ const Orders_Page = () => {
           className={category != "Active" ? "text-gray-400" : ""}
         >
           Active({Active})
+        </h2>
+        <h2
+          role="button"
+          id="OutSourced"
+          onClick={clickHandler}
+          className={category != "OutSourced" ? "text-gray-400" : ""}
+        >
+          OutSourced({OutSourced})
         </h2>
         <h2
           role="button"
@@ -80,21 +121,36 @@ const Orders_Page = () => {
           onClick={clickHandler}
           className={category != "All" ? "text-gray-400" : ""}
         >
-          All({all})
+          All({All})
         </h2>
       </div>
-
+      </div>:
+      <div>
+        <h1 className="text-black mt-6 ml-12 font-bold text-3xl 2xl:text-8xl">Orders</h1>
+      <div>
+  <div className="flex justify-center">
+    <button onClick={setmenu} class="inline-flex  justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
+      Order type 
+      <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+        <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+      </svg>
+    </button>
+  </div>
+  <div id="menu" class={menuClass.class} role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+    <div class="py-1">
+      <p class="text-gray-700 block px-4 py-2 text-sm" role="button" onClick={clickHandler} tabindex="-1" id="Active">Active {Active}</p>
+      <p class="text-gray-700 block px-4 py-2 text-sm" role="button" onClick={clickHandler} tabindex="-1" id="OutSourced">OutSourced {OutSourced}</p>
+      <p class="text-gray-700 block px-4 py-2 text-sm" role="button" onClick={clickHandler} tabindex="-1" id="All">All {All}</p>
+      <p class="text-gray-700 block px-4 py-2 text-sm" role="button" onClick={clickHandler} tabindex="-1" id="Cancelled">Cancelled {Cancelled}</p>
+      <p class="text-gray-700 block px-4 py-2 text-sm" role="button" onClick={clickHandler} tabindex="-1" id="Completed">Completed {Completed}</p>
+    </div>
+  </div>
+      </div> </div>}
       <Orders order={order} />
       <Orders order={order2} />
       <Orders order={order} />
+      <Orders order={order2} />
       <Orders order={order} />
-      <Orders order={order} />
-      {/* <Orders order={order}/> */}
-      {/* <Orders order={order}/> */}
-      {/* <Orders/>
-                  <Orders/>
-                  <Orders/>
-                  <Orders/> */}
     </>
   );
 };
